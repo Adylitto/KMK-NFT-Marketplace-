af62b9ea2a9be98b53e049a65f32de160d3cedf9
adylitto <adylitto@gmail.com>
2022-02-04 17:29:18 +0100

    pageview readme jpg

---
+++ .next/server/chunks/247.js
@@ -0,0 +1,293 @@
+"use strict";
+exports.id = 247;
+exports.ids = [247];
+exports.modules = {
+
+/***/ 48:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var formik__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2296);
+/* harmony import */ var formik__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(formik__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var yup__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5609);
+/* harmony import */ var yup__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(yup__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const initialValues = {
+    fullName: "",
+    email: "",
+    address: "",
+    city: "",
+    country: "",
+    postal: ""
+};
+const PersonalInfoSchema = yup__WEBPACK_IMPORTED_MODULE_2__.object().shape({
+    fullName: yup__WEBPACK_IMPORTED_MODULE_2__.string().required("Full is required"),
+    email: yup__WEBPACK_IMPORTED_MODULE_2__.string().email("Email is invalid").required("Email is required"),
+    address: yup__WEBPACK_IMPORTED_MODULE_2__.string().required("Present Address is required"),
+    city: yup__WEBPACK_IMPORTED_MODULE_2__.string().required("City is required"),
+    postal: yup__WEBPACK_IMPORTED_MODULE_2__.string().required("Post code is required"),
+    country: yup__WEBPACK_IMPORTED_MODULE_2__.string().required("country is required")
+});
+function PersonalInfo() {
+    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Formik, {
+            initialValues: initialValues,
+            validationSchema: PersonalInfoSchema,
+            onSubmit: (fields)=>{
+                alert("SUCCESS!! :-)\n\n" + JSON.stringify(fields, null, 4));
+            },
+            children: ({ errors , status , touched  })=>/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(formik__WEBPACK_IMPORTED_MODULE_1__.Form, {
+                    children: [
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                            className: "row",
+                            children: [
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-xxl-6 col-xl-6 col-lg-6 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Full Name"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "fullName",
+                                            type: "text",
+                                            className: "form-control" + (errors.fullName && touched.fullName ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "fullName",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-xxl-6 col-xl-6 col-lg-6 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Email"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "email",
+                                            type: "text",
+                                            className: "form-control" + (errors.email && touched.email ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "email",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-xxl-6 col-xl-6 col-lg-6 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Address"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "address",
+                                            type: "text",
+                                            className: "form-control" + (errors.address && touched.address ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "address",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-xxl-6 col-xl-6 col-lg-6 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "City"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "city",
+                                            type: "text",
+                                            className: "form-control" + (errors.city && touched.city ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "city",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-xxl-6 col-xl-6 col-lg-6 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Post Code"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "postal",
+                                            type: "text",
+                                            className: "form-control" + (errors.postal && touched.postal ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "postal",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-xxl-6 col-xl-6 col-lg-6 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Country"
+                                        }),
+                                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "country",
+                                            as: "select",
+                                            className: "form-control" + (errors.country && touched.country ? " is-invalid" : ""),
+                                            children: [
+                                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("option", {
+                                                    value: "Bangladesh",
+                                                    children: "Bangladesh"
+                                                }),
+                                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("option", {
+                                                    value: "United States",
+                                                    children: "United States"
+                                                }),
+                                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("option", {
+                                                    value: "United Kingdom",
+                                                    children: "United Kingdom"
+                                                })
+                                            ]
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "country",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: "mt-3",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+                                type: "submit",
+                                className: "btn btn-primary mr-2",
+                                children: "Save"
+                            })
+                        })
+                    ]
+                })
+        })
+    }));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PersonalInfo);
+
+
+/***/ }),
+
+/***/ 9244:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var formik__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2296);
+/* harmony import */ var formik__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(formik__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var yup__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5609);
+/* harmony import */ var yup__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(yup__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const initialValues = {
+    email: "",
+    password: ""
+};
+const UpdateInfoSchema = yup__WEBPACK_IMPORTED_MODULE_2__.object().shape({
+    email: yup__WEBPACK_IMPORTED_MODULE_2__.string().email("Email is invalid").required("Email is required"),
+    password: yup__WEBPACK_IMPORTED_MODULE_2__.string().min(6, "Password must be at least 6 characters").required("Password is required")
+});
+function UpdateInfo() {
+    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Formik, {
+            initialValues: initialValues,
+            validationSchema: UpdateInfoSchema,
+            onSubmit: (fields)=>{
+                alert("SUCCESS!! :-)\n\n" + JSON.stringify(fields, null, 4));
+            },
+            children: ({ errors , status , touched  })=>/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(formik__WEBPACK_IMPORTED_MODULE_1__.Form, {
+                    children: [
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                            className: "row",
+                            children: [
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-12 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Email"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "email",
+                                            type: "text",
+                                            className: "form-control" + (errors.email && touched.email ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "email",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                    className: "col-12 mb-3",
+                                    children: [
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                                            className: "form-label",
+                                            children: "Password"
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.Field, {
+                                            name: "password",
+                                            type: "text",
+                                            className: "form-control" + (errors.password && touched.password ? " is-invalid" : "")
+                                        }),
+                                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(formik__WEBPACK_IMPORTED_MODULE_1__.ErrorMessage, {
+                                            name: "password",
+                                            component: "div",
+                                            className: "invalid-feedback"
+                                        })
+                                    ]
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: "mt-3",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+                                type: "submit",
+                                className: "btn btn-primary mr-2",
+                                children: "Save"
+                            })
+                        })
+                    ]
+                })
+        })
+    }));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (UpdateInfo);
+
+
+/***/ })
+
+};
+;
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # KMK-NFT-Marketplace-
+<img src="https://user-images.githubusercontent.com/9112801/96329524-c8906600-101b-11eb-8fd8-4d45e7f0cd1b.jpg" alt="Preview" style="max-width: 100%;">
